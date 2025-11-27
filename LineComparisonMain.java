638fac5237fd321bd12ba7601034450a79f53a07
krishnach9 <krishna.chandana@bridgelabz.com>
2025-11-27 14:20:40 +0530

    Add files via upload

---
+++ LineComparisonMain.java
@@ -0,0 +1,8 @@
+package LineComparision;
+
+public class LineComparisonMain {
+	 public static void main(String[] args) {
+	        System.out.println("Welcome to Line Comparison Computation Program");
+	    }
+
+}
